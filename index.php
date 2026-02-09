1d85a5996a83be7e3a3bc8742a701badcc1c3551
scorpion7slayer <theodarville@gmail.com>
2026-02-09 13:55:07 +0100

    update

--- index.php
+++ index.php
@@ -24,3 +24,18 @@ if ($user) {
     $isAdmin = false;
   }
 }
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Market Plier</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
