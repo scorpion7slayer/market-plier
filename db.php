181af3f3919036a1488fd4a53c968fd094d1a3e4
scorpion7slayer <theodarville@gmail.com>
2026-02-09 13:26:27 +0100

    update

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$dbHost = '213.32.20.54';
+$dbName = 'marketplier';
+$dbUser = 'root';
+$dbPass = 'HoriMySQL2026!';
+
+$dsn = "mysql:host={$dbHost};dbname={$dbName};charset=utf8mb4";
+
+$pdoOptions = [
+     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+     PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $dbUser, $dbPass, $pdoOptions);
+} catch (PDOException $ex) {
+     throw new PDOException($ex->getMessage(), (int)$ex->getCode());
+}
