1613778f8767385fed051e298f572ba9d7d37c98
scorpion7slayer <120050493+scorpion7slayer@users.noreply.github.com>
2026-02-26 16:02:00 +0100

    feat(database): ajout de la table `listings` avec ses colonnes et contraintes, mise à jour de la table `profile`

--- database/marketplier.sql
+++ database/marketplier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 26 fév. 2026 à 12:29
+-- Généré le : jeu. 26 fév. 2026 à 13:54
 -- Version du serveur : 8.0.45-0ubuntu0.24.04.1
 -- Version de PHP : 8.3.6
 
@@ -24,12 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `listings`
+--
+
+CREATE TABLE `listings` (
+  `id` int UNSIGNED NOT NULL,
+  `auth_token` varchar(64) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) UNSIGNED NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `item_condition` varchar(30) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `profile`
 --
 
 CREATE TABLE `profile` (
-  `description` int NOT NULL
+  `auth_token` varchar(64) NOT NULL,
+  `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `profile`
+--
+
+INSERT INTO `profile` (`auth_token`, `description`) VALUES
+('341f195ce689e978ee8845fbe466e7ba13bd8b87ccc82987f55634c109d96494', 'jadore les pieds'),
+('4337b9b2c6d3633fd055c0c878396ef4b2f83de0a572a036356bcfac74d0480d', '');
 
 -- --------------------------------------------------------
 
@@ -54,12 +82,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `email`, `google_id`, `auth_provider`, `password_hash`, `profile_photo`, `is_admin`, `created_at`, `auth_token`) VALUES
+('Gaspacho', 'milangaspard1302@gmail.com', '112289525325917462978', 'google', NULL, 'user_341f195ce689e978ee8845fbe466e7ba13bd8b87ccc82987f55634c109d96494_1772109983.png', 0, '2026-02-26 12:34:29', '341f195ce689e978ee8845fbe466e7ba13bd8b87ccc82987f55634c109d96494'),
 ('Gaspami', 'gaspami@gmail.com', NULL, 'local', '$2y$12$GCgfAY/6wmA5rQ.BrzYzHO.tFeoWPWP8VI4TpHr.yrpxrSbdKxCjG', 'user_d9f6b978000ec5741ff5d2b886f925b4db46b567701fda41b41e10fef93f6e78_1770967886.png', 0, '2026-02-13 07:31:08', 'd9f6b978000ec5741ff5d2b886f925b4db46b567701fda41b41e10fef93f6e78'),
 ('theo', 'theodarville@gmail.com', '101826161579785365348', 'both', '$2y$12$HVkLWHAeJ54/bawzD8unHelKOmdBwTn56aYEvlPHIYDJNQt5U2ZgO', 'user_4337b9b2c6d3633fd055c0c878396ef4b2f83de0a572a036356bcfac74d0480d_1770968406.png', 0, '2026-02-13 07:28:32', '4337b9b2c6d3633fd055c0c878396ef4b2f83de0a572a036356bcfac74d0480d');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `listings`
+--
+ALTER TABLE `listings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_auth_token` (`auth_token`);
+
+--
+-- Index pour la table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`auth_token`);
 
 --
 -- Index pour la table `users`
@@ -70,6 +112,32 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `auth_token` (`auth_token`),
   ADD UNIQUE KEY `idx_google_id` (`google_id`),
   ADD KEY `idx_auth_token` (`auth_token`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `listings`
+--
+ALTER TABLE `listings`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `listings`
+--
+ALTER TABLE `listings`
+  ADD CONSTRAINT `fk_listing_auth_token` FOREIGN KEY (`auth_token`) REFERENCES `users` (`auth_token`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `fk_profile_auth_token` FOREIGN KEY (`auth_token`) REFERENCES `users` (`auth_token`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
